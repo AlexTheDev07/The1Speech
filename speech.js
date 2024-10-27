ca05e2f4fb87635de95e1a866f854d8d4ad9754e
Alexツ <54591924+AlexTheDev07@users.noreply.github.com>
2024-10-27 15:48:31 +0100

    Test

---
+++ speech.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, onValue } from "firebase/database";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCzv5mbY-F9hHXcWI8-wvyznL4cXQQNUBA",
+    authDomain: "speech-cd68b.firebaseapp.com",
+    databaseURL: "https://speech-cd68b-default-rtdb.firebaseio.com", // Add your database URL
+    projectId: "speech-cd68b",
+    storageBucket: "speech-cd68b.appspot.com",
+    messagingSenderId: "189245008930",
+    appId: "1:189245008930:web:08f95d15ab40f03672ae7b",
+    measurementId: "G-16JL9LCMMF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Load the last transcription from Firebase on page load
+window.onload = function() {
+    const transcriptionRef = ref(db, 'transcription/');
+    onValue(transcriptionRef, (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+            document.getElementById('outputBox').innerHTML = data.text; // Adjusted to display correct data
+        }
+    });
+};
